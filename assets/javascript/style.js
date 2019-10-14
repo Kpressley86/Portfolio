4d8bfb5ecabdc42f871dd4b22c13609e5a3317bd
Kelly Pressley <kellypressley86@gmail.com>
2019-10-14 15:40:59 -0700

    added crystal link

--- assets/javascript/style.js
+++ assets/javascript/style.js
@@ -1,20 +1,20 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     // HIDES SMOKE DIV FOR BETTER DISPLAY //
-    $(function() {
-        setTimeout(function() {
+    $(function () {
+        setTimeout(function () {
             $("#smoke").hide('blind', {}, 500)
         }, 5000);
     });
 
     // SLIDE IN HIDDEN CLASSES //
-    $('.hidden').hide("fast",function(){
+    $('.hidden').hide("fast", function () {
         $(".hidden").show(6000);
-        });
+    });
 
-   
-   $("#logo").click(function(){
-    document.location.href = "./index.html";
- });
+
+    $("#logo").click(function () {
+        document.location.href = "./index.html";
+    });
 
 });
